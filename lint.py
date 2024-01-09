58431d8291f8307214ed28d61f94878531d25516
= <=>
2024-01-09 15:36:18 +0000

    A New Hope 31

--- lint.py
+++ lint.py
@@ -3,7 +3,9 @@ from pylint.lint import Run
 
 THRESHOLD = 9
 
-run = Run(["factorial.py"],do_exit=False)
+print("something bla")
+
+run = Run(["factorial.py"])
 
 score = run.linter.stats["global_note"]
 
@@ -13,8 +15,5 @@ if score < THRESHOLD:
     """Linter message."""
     print("Linter failed: Score < threshold value")
     sys.exit(1)
-
-print("something bla")
-
 sys.exit(0)
 
