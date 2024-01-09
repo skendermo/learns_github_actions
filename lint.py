7f77b3cd9b9b08432f44828954c956d1444cb936
= <=>
2024-01-09 15:08:02 +0000

    A New Hope 19

--- lint.py
+++ lint.py
@@ -7,8 +7,9 @@ run = lint.Run(["factorial.py"])
 
 score = run.linter.stats["global_note"]
 
+print(score)
+
 if score > THRESHOLD:
     """Linter message."""
     print("Linter failed: Score < threshold value")
-
     sys.exit(1)
